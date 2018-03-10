5d01e36243b179b4c8a64531f480671f33d3af11
Fakher Oueslati <oueslati.fakher@gmail.com>
2018-03-10 10:14:06 -0500

    feat: Added feature 2

--- publish_lambda.sh
+++ publish_lambda.sh
@@ -3,4 +3,4 @@ cd lambda
 npm install
 zip -r ../lambda.zip *
 cd ..
-aws lambda update-function-code --function-name YOUR_FUNCTION_NAME --zip-file fileb://lambda.zip
+aws lambda update-function-code --function-name alexa-meetup --zip-file fileb://lambda.zip --profile ofa5er-default
